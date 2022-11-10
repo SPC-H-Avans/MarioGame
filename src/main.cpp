45e91366799239b4ecaec609d8687453c6e9faa2
Jaap <jaap@mrproper.dev>
2022-11-10 11:26:40 +0100

    Magic numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "Game/Game.h"
 
+const int VIEW_HEIGHT = 500;
+const int VIEW_WIDTH = 500;
+
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int {
-  PlatformerGame::Game(500, 500);
+  PlatformerGame::Game(VIEW_HEIGHT, VIEW_WIDTH);
   return 0;
 }
