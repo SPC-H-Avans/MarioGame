3f3ba52cf2a33b832291ac53a190335aa662a5ef
Jaap <jaap@mrproper.dev>
2022-11-17 18:18:43 +0100

    Window name

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ const int VIEW_WIDTH = 500;
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int {
     platformer_engine::Engine &engine = platformer_engine::Engine::GetInstance();
-    engine.Init(VIEW_WIDTH, VIEW_HEIGHT, "PlatFormer Engine Debug", spic::Color::Green());
+    engine.Init(VIEW_WIDTH, VIEW_HEIGHT, "Mario Game - Platformer Engine", spic::Color::Green());
     platformer_engine::TextureManager::GetInstance().LoadTexture("mario_Jump", "./resources/Sprites/Mario/Walk.png");
     GameObjectBuilder gameObjectBuilder {"speler"};
     auto animatedSprite = std::make_shared<platformer_engine::AnimatedSprite>("mario_Jump", 1, 1, 24, 24, 3, 100, 1, platformer_engine::FLIP_NONE, Color::Transparent(), 4.0);
