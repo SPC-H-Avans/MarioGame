05cebadaac8fca7ec7892d64b8d492223ebe0a51
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-11-17 10:44:16 +0100

    game klopt weer inclusief engine

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -2,6 +2,6 @@
 
 namespace PlatformerGame {
     Game::Game(int viewHeight, int viewWidth) {
-    platformer_engine::Engine::GetInstance().Init(viewWidth, viewHeight, "Mario Game", spic::Color::Green());
+        platformer_engine::Engine::GetInstance().Init(viewWidth, viewHeight, "Mario Game", spic::Color::Green());
     }
 } // PlatformerGame
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@ const int VIEW_HEIGHT = 500;
 const int VIEW_WIDTH = 500;
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int {
-  PlatformerGame::Game(VIEW_HEIGHT, VIEW_WIDTH);
-  return 0;
+
+    platformer_engine::Engine &engine = platformer_engine::Engine::GetInstance();
+    engine.Init(VIEW_WIDTH, VIEW_HEIGHT, "PlatFormer Engine Debug", spic::Color::Green());
+    engine.Start();
+
+
+    return 0;
 }
