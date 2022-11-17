274e88aabb1b79a62a9b22142ac09cad04a55a60
Wesley Salimans <wlt.salimans@student.avans.nl>
2022-11-17 10:44:24 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,9 @@ const int VIEW_HEIGHT = 500;
 const int VIEW_WIDTH = 500;
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int {
-
     platformer_engine::Engine &engine = platformer_engine::Engine::GetInstance();
     engine.Init(VIEW_WIDTH, VIEW_HEIGHT, "PlatFormer Engine Debug", spic::Color::Green());
     engine.Start();
-
 
     return 0;
 }
