31f467a328d7b3ba97d09604e7f4241d83dea3a0
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-11-23 13:56:55 +0100

    Update Game.cpp

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.hpp"
 #include "Builder/GameObjectBuilder.hpp"
 #include "Builder/SceneBuilder.hpp"
+#include "Director/GameObjectDirector.hpp"
 
 namespace PlatformerGame {
     Game::Game(int viewWidth, int viewHeight) {
@@ -10,7 +11,12 @@ namespace PlatformerGame {
         platformer_engine::SceneBuilder builder = platformer_engine::SceneBuilder();
         engine.SetActiveScene(builder.GetScene());
 
-        engine.GetActiveScene()->ImportLevel("map1","./resources/levels/mario/", "map1.tmx");
+        std::map<int, std::function<std::shared_ptr<GameObject>()>> config { // TODO: find a better place to put this
+                {4, [](){ return GameObjectDirector::CreateTile();}}, // 4 is breakable block
+        };
+        auto test = config.at(4)();
+
+        engine.GetActiveScene()->ImportLevel("map1","./resources/levels/mario/", "map1.tmx", config);
         std::unique_ptr<Scene>& scene =  engine.GetActiveScene();
         platformer_engine::TextureManager::GetInstance().LoadTexture("mario_Jump", "./resources/Sprites/Mario/Walk.png");
         GameObjectBuilder gameObjectBuilder{"speler"};
@@ -19,15 +25,10 @@ namespace PlatformerGame {
                                                                           Color::Transparent(), 1.0);
         gameObjectBuilder.AddAnimator(sprite);
         auto gameObject = gameObjectBuilder.GetGameObject();
-        auto transform = Transform();
-        transform.position = Point();
-        transform.rotation = 0;
-        transform.position.x = 100;
-        transform.position.y = 250;
-        transform.scale = 1.0;
+        auto transform = Transform { Point {100, 250}, 0, 1.0 };
         gameObject->SetTransform(transform);
         scene->AddObject(gameObject);
 
         engine.Start();
     }
-} // PlatformerGame
+}  // namespace PlatformerGame
