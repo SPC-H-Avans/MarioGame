91d211cfac9f03bff45d10069f2abe9430ce6360
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-11-23 22:07:07 +0100

    can load blocks

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -10,14 +10,25 @@ namespace PlatformerGame {
 
         platformer_engine::SceneBuilder builder = platformer_engine::SceneBuilder();
         engine.SetActiveScene(builder.GetScene());
+        std::unique_ptr<Scene>& scene =  engine.GetActiveScene();
 
-        std::map<int, std::function<std::shared_ptr<GameObject>()>> config { // TODO: find a better place to put this
-                {4, [](){ return GameObjectDirector::CreateTile();}}, // 4 is breakable block
+        //
+        platformer_engine::TextureManager::GetInstance().LoadTexture("goomba", "./resources/Sprites/Goomba/Idle.png");
+        auto sprite4 = std::make_shared<spic::Sprite>("goomba", 1, 1, 24, 24,
+                                                     platformer_engine::FLIP_NONE,
+                                                     Color::Transparent(), 1.0);
+        platformer_engine::TextureManager::GetInstance().LoadTexture("floor1", "./resources/levels/mario/Tilesets/Tiles/floor1.png");
+        auto sprite1 = std::make_shared<spic::Sprite>("floor1", 1, 1, 16, 16,
+                                                      platformer_engine::FLIP_NONE,
+                                                      Color::Transparent(), 1.0);
+
+        std::map<int, std::function<std::shared_ptr<GameObject>(Transform)>> config { // TODO: find a better place to put this
+                {4, [&sprite4](Transform transform){ return GameObjectDirector::CreateTile(sprite4, transform);}}, // 4 is breakable block
+                {1, [&sprite1](Transform transform){ return GameObjectDirector::CreateTile(sprite1, transform);}}, // 4 is breakable block
         };
-        auto test = config.at(4)();
+        //
 
         engine.GetActiveScene()->ImportLevel("map1","./resources/levels/mario/", "map1.tmx", config);
-        std::unique_ptr<Scene>& scene =  engine.GetActiveScene();
         platformer_engine::TextureManager::GetInstance().LoadTexture("mario_Jump", "./resources/Sprites/Mario/Walk.png");
         GameObjectBuilder gameObjectBuilder{"speler"};
         auto sprite = std::make_shared<platformer_engine::AnimatedSprite>("mario_Jump", 1, 1, 24, 24, 3, 100, 1,
