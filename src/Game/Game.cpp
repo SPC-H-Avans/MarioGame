87a4306cbe239f7c13734839427681d600467e7c
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-11-28 15:22:23 +0100

    changes after merge to build the game

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -8,11 +8,12 @@ namespace PlatformerGame {
         platformer_engine::Engine &engine = platformer_engine::Engine::GetInstance();
         engine.Init(viewWidth, viewHeight, "Mario Game", spic::Color::Cyan());
 
-        platformer_engine::SceneBuilder builder = platformer_engine::SceneBuilder();
-        engine.SetActiveScene(builder.GetScene());
-        std::unique_ptr<Scene>& scene =  engine.GetActiveScene();
+        platformer_engine::SceneBuilder builder = platformer_engine::SceneBuilder("level1");
+        engine.AddScene(builder.GetScene());
+        engine.SetActiveScene("level1");
+        auto scene =  engine.GetActiveScene();
 
-        engine.GetActiveScene()->ImportLevel("map1","./resources/levels/mario/", "map1.tmx", TileConfig::Level1());
+        engine.GetActiveScene().ImportLevel("map1","./resources/levels/mario/", "map1.tmx", TileConfig::Level1());
         platformer_engine::TextureManager::GetInstance().LoadTexture("mario_Jump", "./resources/Sprites/Mario/Walk.png");
         GameObjectBuilder gameObjectBuilder{"speler"};
         auto sprite = std::make_shared<platformer_engine::AnimatedSprite>("mario_Jump", 1, 1, 24, 24, 3, 100, 1,
@@ -22,7 +23,7 @@ namespace PlatformerGame {
         auto gameObject = gameObjectBuilder.GetGameObject();
         auto transform = Transform { Point {100, 250}, 0, 1.0 };
         gameObject->SetTransform(transform);
-        scene->AddObject(gameObject);
+        scene.AddObject(gameObject);
 
         engine.Start();
         std::cout<<1;
