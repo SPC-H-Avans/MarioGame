c110668685192ba987c3f4fd9527b8cfe5c7b50b
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-11-29 13:02:47 +0100

    updated animations

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -16,15 +16,24 @@ namespace PlatformerGame {
         Scene::ImportLevel("map1","./resources/levels/mario/", "map1.tmx", TileConfig::Level1());
 //        Scene::ImportLevel("map1","./resources/levels/mario/", "map2.tmx", TileConfig::Level1());
 
-        platformer_engine::TextureManager::GetInstance().LoadTexture("mario_Jump", "./resources/Sprites/Mario/Walk.png");
         GameObjectBuilder gameObjectBuilder{"speler"};
+        platformer_engine::TextureManager::GetInstance().LoadTexture("mario_idle", "./resources/Sprites/Mario/Idle.png");
+        platformer_engine::TextureManager::GetInstance().LoadTexture("mario_walk", "./resources/Sprites/Mario/Walk.png");
+        platformer_engine::TextureManager::GetInstance().LoadTexture("mario_jump", "./resources/Sprites/Mario/Jump.png");
         int w = 15;
         int h = 17;
-        auto sprite = std::make_shared<platformer_engine::AnimatedSprite>("mario_Jump", 1, 1, w, h, 3, 100, 1,
-                                                                          platformer_engine::FLIP_NONE,
-                                                                          Color::Transparent(), 1.0);
+        auto idleSprite = std::make_shared<platformer_engine::AnimatedSprite>("mario_idle", 1, 1, w, h, 1, 100, 1,
+                                                                              platformer_engine::FLIP_NONE,
+                                                                              Color::Transparent(), 1.0);
+        auto walkSprite = std::make_shared<platformer_engine::AnimatedSprite>("mario_walk", 1, 1, w, h, 3, 100, 1,
+                                                                              platformer_engine::FLIP_NONE,
+                                                                              Color::Transparent(), 1.0);
+        auto jumpSprite = std::make_shared<platformer_engine::AnimatedSprite>("mario_jump", 1, 1, w + 1, h - 1, 1, 100, 1,
+                                                                              platformer_engine::FLIP_NONE,
+                                                                              Color::Transparent(), 1.0);
+
         auto transform = Transform { Point {100, 250}, 0, 1.0 };
-        auto mario = GameObjectDirector::CreatePlayer(sprite, transform, w, h);
+        auto mario = GameObjectDirector::CreatePlayer(idleSprite, walkSprite, jumpSprite, transform, w, h);
 
         engine.Start();
         std::cout<<1;
