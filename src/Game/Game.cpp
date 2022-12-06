4be534806abe9726a035c2425aa72ea93cb2d71f
Jesse <55792758+Nich-Thompson@users.noreply.github.com>
2022-12-06 17:52:18 +0100

    added test UI objects

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -41,7 +41,6 @@ namespace PlatformerGame {
         auto jumpSprite = platformer_engine::AnimatedSprite("mario_jump", w + 1, h - 1, 1, 1, 1, 100,
                                                             platformer_engine::FLIP_HORIZONTAL); // 16x16 // TODO: fix flip
 
-        auto transform = Transform{Point{100, 250}, 0, 1.0};
         auto animations = std::vector<platformer_engine::AnimatedSprite>{idleSprite, walkSprite, jumpSprite};
         auto behaviourScripts = std::vector<std::shared_ptr<spic::BehaviourScript>>{
                 std::make_shared<platformer_engine::CollisionBehaviour>(),
@@ -49,15 +48,46 @@ namespace PlatformerGame {
                 std::make_shared<PlatformerGame::DynamicAnimationBehaviour>(idleSprite, walkSprite, jumpSprite)
         };
 
-        auto mario = GameObjectDirector::CreatePlayer(transform, w, h - 1, animations, behaviourScripts);
+        auto mario = GameObjectDirector::CreatePlayer(
+                Transform{Point{340, 100}, 0, 1.0},
+                w, h - 1, animations, behaviourScripts);
 
         camera.SetTarget(mario);
 
         scene.AddCamera(camera);
 
-        GameObjectDirector::CreateText(transform, "coins", "COINS: x01",
-                                       "./resources/fonts/DefaultFont.ttf", 100, 50,
-                                       24, Color::Yellow());
+        // test Text
+        auto textId = "coins";
+        auto text = "COINS: x00";
+        auto fontPath = "./resources/fonts/DefaultFont.ttf";
+        auto fontSize = 24;
+        auto color = Color::Yellow();
+        GameObjectDirector::CreateText(
+                Transform{Point{300, 0}, 0, 1.0},
+                textId,
+                text,
+                fontPath,
+                100, 50,
+                fontSize, color);
+
+        // test update text
+        text = "COINS: x99";
+        platformer_engine::TextureManager::GetInstance().CreateOrUpdateUIText(textId, fontPath, text, fontSize, color);
+
+        // test Button
+        const auto textureName = "startButton";
+        const auto spriteSize = 16;
+        const auto spriteScale = 4;
+
+        auto buttonSprite = spic::Sprite(textureName, spriteSize, spriteSize);
+        buttonSprite.SetSpriteScale(spriteScale);
+        GameObjectDirector::CreateButton(
+                Transform{Point{50, 50}, 0, spriteScale},
+                "button1",
+                buttonSprite,
+                "./resources/UI/start.png",
+                spriteSize * spriteScale, spriteSize * spriteScale,
+                []{ std::cout << "click" << std::endl; });
 
         engine.Start();
         std::cout << 1;
