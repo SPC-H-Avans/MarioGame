d8b874491bc8f5f69c50c86ccdd4eed0d1e367b4
Jaap <jaap@mrproper.dev>
2022-12-08 13:53:14 +0100

    Name

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -12,14 +12,12 @@ namespace PlatformerGame {
     Game::Game(int viewWidth, int viewHeight) {
         platformer_engine::Engine &engine = platformer_engine::Engine::GetInstance();
         engine.Init(viewWidth, viewHeight, "Mario Game", spic::Color::Cyan());
+        
+       NetworkingServer networkingServer;
+       networkingServer.CreateServer("level1", 10, 7778);
 
-
-     //   NetworkingServer networkingServer;
-      //  networkingServer.CreateServer("level1", 10, 7778);
-
-      NetworkingClient networkingClient;
-      networkingClient.ConnectToServer("192.168.68.107", 7778);
-
+      //NetworkingClient networkingClient;
+      //networkingClient.ConnectToServer("192.168.68.107", 7778);
 
         engine.Start();
         std::cout<<1;
