546ac6509e3a379a646b3b38301c576bd9309aeb
Niels van Hal <ntj.vanhal@student.avans.nl>
2022-12-12 10:18:11 +0100

    Scene change

--- src/Scripts/FlagBehaviour.cpp
+++ src/Scripts/FlagBehaviour.cpp
@@ -1,13 +1,23 @@
 
 #include "FlagBehaviour.hpp"
 #include "GameObject.hpp"
+#include "Engine/Engine.hpp"
 
 namespace PlatformerGame {
     void FlagBehaviour::OnTriggerEnter2D(Collision collision) {
         auto collidingObject = collision.GetCollider()->GetGameObject().lock();
 
         if(collidingObject->GetTag() == "player") {
-             //GameOver
+            auto& engine = platformer_engine::Engine::GetInstance();
+            auto nextScene = engine.GetActiveScene().GetNextScene();
+            std::string next;
+
+            if(nextScene.has_value()) {
+                next = nextScene.value();
+                engine.SetActiveScene(next);
+            } else {
+                engine.SetActiveScene(engine.GetDefaultSceneName());
+            }
         }
     }
 }
