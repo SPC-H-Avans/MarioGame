cf60443ae52c69faa1cd65bb83519886640806aa
Jaap <jaap@mrproper.dev>
2022-12-19 12:44:28 +0100

    Star fix

--- src/Networking/NetworkingClient.cpp
+++ src/Networking/NetworkingClient.cpp
@@ -39,8 +39,19 @@ void PlatformerGame::NetworkingClient::ConnectToServer(const std::string &server
         auto jumpSprite = platformer_engine::AnimatedSprite("mario_jump", w + 1, h - 1, 1, 1, 1, 100,
                                                             platformer_engine::FLIP_HORIZONTAL); // 16x16 // TODO: fix flip
 
+
+            platformer_engine::TextureManager::GetInstance().LoadTexture("mario_idle_star",
+                                                                         "./resources/Sprites/Mario/Star/Idle.png");
+            platformer_engine::TextureManager::GetInstance().LoadTexture("mario_walk_star",
+                                                                         "./resources/Sprites/Mario/Star/Walk.png");
+            platformer_engine::TextureManager::GetInstance().LoadTexture("mario_jump_star",
+                                                                         "./resources/Sprites/Mario/Star/Jump.png");
+            auto idleStarSprite = platformer_engine::AnimatedSprite("mario_idle_star", w, h, 6);
+            auto walkStarSprite = platformer_engine::AnimatedSprite("mario_walk_star", w, h, 6);
+            auto jumpStarSprite = platformer_engine::AnimatedSprite("mario_jump_star", w + 1, h - 1, 6);
+
         auto transform = Transform{Point{100, 220}, 0, 1.0};
-        auto animations = std::vector<platformer_engine::AnimatedSprite>{idleSprite, walkSprite, jumpSprite};
+        auto animations = std::vector<platformer_engine::AnimatedSprite>{walkSprite, jumpSprite, idleStarSprite, walkStarSprite, jumpStarSprite, idleSprite};
         auto behaviourScripts = std::vector<std::shared_ptr<spic::BehaviourScript>>{
             std::make_shared<platformer_engine::CollisionBehaviour>(),
             std::make_shared<PlatformerGame::MarioBehaviour>()
