75c68c8977c78c0121c2380f34c51fb4f0877b2c
Jaap <jaap@mrproper.dev>
2022-12-20 16:28:06 +0100

    Nullptr check

--- src/Scripts/MarioBehaviour.cpp
+++ src/Scripts/MarioBehaviour.cpp
@@ -20,7 +20,7 @@ namespace PlatformerGame {
 
     void MarioBehaviour::OnTriggerEnter2D(Collision collision) {
         auto collidingObject = collision.GetCollider()->GetGameObject().lock();
-
+        if(collidingObject == nullptr) return;
         if(collidingObject->GetTag() == "enemy") { //Might expand this in future
             _state->TakeDamage();
         }
